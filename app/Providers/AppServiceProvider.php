959a5dcff9bafd71636e77519b84924f613f6818
allaninfo <allan@allanbr.net>
2021-03-12 01:14:19 -0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if (app()->environment('production')) {
+            URL::forceSchema('https');
+        }
+
         //Menu
 
         $frontMenu = [
